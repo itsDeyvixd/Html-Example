b68ab4b3354e78c5dd08a6b3eed197eeb77cc337
Deyvi <dfrioman@gmail.com>
2024-09-23 20:40:38 -0500

    9.2 POO Principles added

--- funcion.js
+++ funcion.js
@@ -77,22 +77,25 @@ function ejFor() {
     }
 
     console.log("x al final:" + x);
-}
+}``
 
 class Carro {
+
+    #color; // Remove the empty declaration, it's not valid in JavaScript
+    
     constructor(pColor, pMarca, pModelo, pPrecio) {
-        this.color = pColor;
+        this.#color = pColor;
         this.marca = pMarca;
         this.modelo = pModelo;
         this.precio = pPrecio;
     }
 
     getColor() {
-        return this.color;
+        return this.#color;
     }
 
     setColor(psColor) {
-        this.color = psColor;
+        this.#color = psColor;
     }
 
     getMarca() {
@@ -130,3 +133,82 @@ class Carro {
         console.log("carro prendido");
     }
 }
+
+class  CarroElectrico extends Carro {
+
+    constructor( pColor, pMarca, pModelo, pPrecio, pBateria) {
+        super(pColor, pMarca, pModelo, pPrecio);
+        this.bateria = pBateria;
+    }
+
+    getBateria() {
+        return this.bateria;
+    }
+    setBateria(){
+        this.bateria = pBateria;
+    }
+
+    cargaBateria(){
+        console.log("Bateria actual :"+this.bateria+"Kw/h");
+    }
+
+}
+
+class  CarroCombustion extends Carro {
+    constructor( pColor, pMarca, pModelo, pPrecio, pCombustible) {
+        super(pColor, pMarca, pModelo, pPrecio);
+        this.combustible = pCombustible;
+}
+
+getCombustible(){
+    return this.combustible;
+}
+
+setCombustible(){
+    this.combustible = pCombustible;
+}
+acelerar(){
+    console.log(this.marca +" acelera de  0 a 100 en 5 segundos ");
+    }
+}
+
+
+
+function ejPoo() {
+
+    var carro1 = new Carro("Rojo", "Ford", "2025", 20000);
+    console.log("Ford color : "  + carro1.getColor());
+    console.log("Ford marca : "  + carro1.getMarca());
+    console.log("Ford modelo : "  + carro1.getModelo());
+    console.log("Ford precio : "  + carro1.getPrecio());
+
+    console.log("---------------------------------------");
+    var carro2 = new CarroElectrico("Azul", "Tesla", "2025", 30000, 150);
+    console.log("Tesla color : "  + carro2.getColor());
+    console.log("Tesla marca : "  + carro2.getMarca());
+    console.log("Tesla modelo : "  + carro2.getModelo());
+    console.log("Tesla precio : "  + carro2.getPrecio());
+    console.log("Tesla bateria : "  + carro2.getBateria());
+    carro2.cargaBateria();
+
+    console.log("---------------------------------------");
+    var carro3 = new CarroCombustion("Negro", "Chevrolet","2024",  25000, "Gasolina");
+    console.log("Chevrolet color : "  + carro3.getColor());
+    console.log("Chevrolet marca : "  + carro3.getMarca());
+    console .log("Chevrolet modelo : "  + carro3.getModelo());
+    console.log("Chevrolet precio : "  + carro3.getPrecio());
+    console.log("Chevrolet combustible : "  + carro3.getCombustible());
+    carro3.acelerar();
+
+
+
+}
+function  ejPoo2() {
+    var carro2 = new Carro("Azul", "Chevrolet", "2025", 30000);
+        console.log("Chevrolet color : "  + carro2.getColor());
+        console.log("Chevrolet marca : "  + carro2.getMarca());
+        console.log("Chevrolet modelo : "  + carro2.getModelo());
+        console.log("Chevrolet precio : "  + carro2.getPrecio());
+        
+    }
+
